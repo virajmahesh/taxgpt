6e358d5c54a876bf46b1c2234a6c0baed6876b79
Viraj Mahesh <virajmahesh@gmail.com>
2024-01-14 09:07:02 -0800

    Load statute text into a SQLite database

--- statute.py
+++ statute.py
@@ -10,6 +10,7 @@ import os
 import re
 from openai import OpenAI
 from bs4 import BeautifulSoup
+from sqlmodel import SQLModel, Field, create_engine, Session
 
 
 # Config
@@ -17,6 +18,16 @@ DATA_DIR = "data"
 TAX_CODE_XHTML_PATH = f"{DATA_DIR}/title26.htm"
 EMBED_MODEL = ""  # Set this explicitly so that it doesn't change automatically
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+
+class Statute(SQLModel, table=True):
+    """
+    A model for a statute in the US tax code.
+    """
+    id: int = Field(default=None, primary_key=True)
+    section: str = Field(default=None)
+    title: str = Field(default=None)
+    text: str = Field(default=None)
 
 
 def split_tax_code(path: str = TAX_CODE_XHTML_PATH, out_dir=DATA_DIR) -> None:
@@ -55,7 +66,9 @@ def split_tax_code(path: str = TAX_CODE_XHTML_PATH, out_dir=DATA_DIR) -> None:
 
         for i, (h, s) in enumerate(zip(headings, statute_bodies)):
             with open(f"{out_dir}/{i}.txt", "w") as f:
-                f.write(get_text_from_html(h))
+                text = get_text_from_html(h)
+                text = text.lstrip().rstrip()
+                f.write(text)
                 f.write("\n")
                 f.write(get_text_from_html(s))
                 f.close()
@@ -63,9 +76,41 @@ def split_tax_code(path: str = TAX_CODE_XHTML_PATH, out_dir=DATA_DIR) -> None:
 
 def load_into_db(path: str = DATA_DIR) -> None:
     """
+    Load the statute text at :path: into a SQLite database.
     """
+    # Create the database and table
+    engine = create_engine('sqlite:///statute.db')
+    SQLModel.metadata.create_all(engine)
+
+    # Load the statutes and add them to the table
+    with Session(engine) as session:
+        # Get a list of .txt files in :path:
+        files = [f for f in os.listdir(path) if f.endswith(".txt")]
+        
+        # Convert the file name to an integer for sorting. Select
+        # everything before the period and convert it to an integer.
+        file_name_to_text = lambda x: int(x.split(".")[0])
+
+        # Load each of the files into the database
+        for f in sorted(files, key=file_name_to_text):
+            with open(f"{path}/{f}", "r") as statute_file:
+                first_line = statute_file.readline()
+
+                # The first line of the file looks like §{section_number} {section_title}
+                result = re.search(r"§(\S+\.\S+) (.*)", first_line)
+                if result is None:
+                    result = re.search(r"§(\S+)\. (.*)", first_line)
+                section, title = result.groups()
+                title = title.strip()
+
+                # Read the rest of the file, and remove any leading or trailing whitespace
+                text = statute_file.read().strip()
+                statute = Statute(section=section, title=title, text=text)
+
+                # Add the statute to the database
+                session.add(statute)
+        session.commit() # Commit the changes
     
 
-
 if __name__ == "__main__":
-    split_tax_code()
+    load_into_db()
