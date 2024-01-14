da9dcd68d016b9131f0f8ae7c21d6a5783c2cd87
Viraj Mahesh <virajmahesh@gmail.com>
2024-01-14 10:14:48 -0800

    Add functions to generate embedding and visualize token count.

--- statute.py
+++ statute.py
@@ -8,26 +8,43 @@
 
 import os
 import re
+import tiktoken
+import numpy as np
 from openai import OpenAI
 from bs4 import BeautifulSoup
-from sqlmodel import SQLModel, Field, create_engine, Session
+import matplotlib.pyplot as plt
+from sqlmodel import SQLModel, Field, create_engine, Session, select
 
 
-# Config
+#########################
+# Constants and Globals #
+#########################
+
+# File paths
 DATA_DIR = "data"
 TAX_CODE_XHTML_PATH = f"{DATA_DIR}/title26.htm"
-EMBED_MODEL = ""  # Set this explicitly so that it doesn't change automatically
+
+# OpenAI Config
+EMBED_MODEL = "text-embedding-ada-002"  # Set this explicitly so that it doesn't change automatically
+encoder = tiktoken.encoding_for_model(EMBED_MODEL)
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+# SQL Enginge for SQLModel
+SQL_ENGINE = "sqlite"
+DB_PATH = "statute.db"
+SQL_ENGINE_PATH = f"{SQL_ENGINE}:///{DB_PATH}"
 
 
 class Statute(SQLModel, table=True):
     """
     A model for a statute in the US tax code.
     """
+
     id: int = Field(default=None, primary_key=True)
     section: str = Field(default=None)
     title: str = Field(default=None)
     text: str = Field(default=None)
+    token_count: int = Field(default=0)
 
 
 def split_tax_code(path: str = TAX_CODE_XHTML_PATH, out_dir=DATA_DIR) -> None:
@@ -78,15 +95,27 @@ def load_into_db(path: str = DATA_DIR) -> None:
     """
     Load the statute text at :path: into a SQLite database.
     """
+
+    def get_section_and_title(first_line: str) -> tuple:
+        """
+        Extract the section number and title from the first line of the statute.
+        """
+        result = re.search(r"§(\S+\.\S+) (.*)", first_line)
+        if result is None:
+            result = re.search(r"§(\S+)\. (.*)", first_line)
+        section, title = result.groups()
+        title = title.strip()
+        return section, title
+
     # Create the database and table
-    engine = create_engine('sqlite:///statute.db')
+    engine = create_engine(SQL_ENGINE_PATH)
     SQLModel.metadata.create_all(engine)
 
     # Load the statutes and add them to the table
     with Session(engine) as session:
         # Get a list of .txt files in :path:
         files = [f for f in os.listdir(path) if f.endswith(".txt")]
-        
+
         # Convert the file name to an integer for sorting. Select
         # everything before the period and convert it to an integer.
         file_name_to_text = lambda x: int(x.split(".")[0])
@@ -98,20 +127,42 @@ def load_into_db(path: str = DATA_DIR) -> None:
 
                 # Extract the section number and title from the first line.
                 # The first line of the file looks like §{section_number} {section_title}
-                result = re.search(r"§(\S+\.\S+) (.*)", first_line)
-                if result is None:
-                    result = re.search(r"§(\S+)\. (.*)", first_line)
-                section, title = result.groups()
-                title = title.strip()
+                section, title = get_section_and_title(first_line)
 
                 # Read the rest of the file, and remove any leading or trailing whitespace
                 text = statute_file.read().strip()
-                statute = Statute(section=section, title=title, text=text)
+                token_count = len(encoder.encode(text))
+                statute = Statute(
+                    section=section, title=title, text=text, token_count=token_count
+                )
 
                 # Add the statute to the database
                 session.add(statute)
-        session.commit() # Commit the changes
-    
+        session.commit()
+
+
+def visualize_token_count() -> None:
+    """
+    Visualize the distribution of token counts in the statutes.
+    """
+    engine = create_engine(SQL_ENGINE_PATH)
+    with Session(engine) as session:
+        statutes = session.exec(select(Statute)).all()
+        token_counts = [s.token_count for s in statutes]
+        plt.hist(token_counts, bins=100)
+        plt.xlabel("Token Count")
+        plt.ylabel("Frequency")
+        plt.title("Distribution of Token Counts in Statutes")
+        plt.show()
+
+
+def embed_text_openai(s: str, model: str = EMBED_MODEL) -> np.ndarray:
+    """
+    Generate a vector embedding for :s: using the OpenAI API.
+    """
+    response = client.embeddings.create(input=s, model=model)
+    return np.asarray(response.data[0].embedding)
+
 
 if __name__ == "__main__":
-    load_into_db()
+    visualize_token_count()
